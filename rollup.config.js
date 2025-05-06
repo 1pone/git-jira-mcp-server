0623b64d54e73cdb2037ff25e5c6af88e0105d47
onePone <pwang31@trip.com>
2025-05-06 21:35:26 +0800

    fix: shebang

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import esbuild from 'rollup-plugin-esbuild';
+import banner2 from 'rollup-plugin-banner2';
 
 export default {
   input: 'src/index.ts', // 你的入口文件
   output: {
     file: 'dist/index.js',
     format: 'esm', // 你用的是 import/export，建议用 esm
-    sourcemap: false,
   },
   plugins: [
     nodeResolve({
@@ -19,6 +19,7 @@ export default {
     esbuild({
       target: 'node18', // 你的 Node 版本
     }),
+    banner2(() => '#!/usr/bin/env node\n', { raw: true }),
   ],
   external: [], // 不排除任何依赖，全部打包进 dist
 };
